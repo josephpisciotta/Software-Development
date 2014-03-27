71163fa615f5ae37ad380500016cbdc40d48da69
Joseph Pisciotta <josephpisciotta@mac.com>
2014-03-26 20:22:45 -0400

    Update to Advisor

--- CharliePlugins/src/charlie/advisor/Advisor.java
+++ CharliePlugins/src/charlie/advisor/Advisor.java
@@ -24,23 +24,28 @@ public class Advisor implements IAdvisor{
      */
     @Override
     public Play advise(Hand myHand, Card upCard){
-        
+        // Left side of table
         if(upCard.value() <= 6){
-            if(myHand.getValue() >= 13){
+            // Your hand >= 12
+            if(myHand.getValue() >= 12){
+                if(upCard.value() <= 3 && myHand.getValue() == 12)
+                    return Play.HIT;
                 return Play.STAY;
             }
-            if(myHand.getValue() <= 12 && myHand.getValue() >= 5){
+            // Between 5 and 12
+            if(myHand.getValue() < 12 && myHand.getValue() >= 5){
+                // Between 9 and 11 unless that one hit space
                 if(myHand.getValue() <= 11 && myHand.getValue() >= 9){
                     if(upCard.value() == 2 && myHand.getValue() == 9)
                         return Play.HIT;
                     return Play.DOUBLE_DOWN;
                 }
-                if((upCard.value() <= 2 && myHand.getValue() == 12) || myHand.getValue() <= 8)
-                    return Play.HIT;
-            }
+                return Play.HIT;
+            }            
             
             return Play.STAY;
         }
+        // Right side of table
         else{
             if(myHand.getValue() >= 17){
                 return Play.STAY;
